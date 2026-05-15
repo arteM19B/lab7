111beabfa07a256b8ab096fc659c559fe843a879
arteM19B <bahetkin.artem@gmail.com>
2026-05-15 18:06:41 +0300

    прокинуты логин и пароль в запросе

--- common/src/main/java/Network/Request.java
+++ common/src/main/java/Network/Request.java
@@ -6,16 +6,24 @@ public class Request implements Serializable {
     private final CommandType commandType;
     private final CommandArgument commandArgument;
     private final String requestId;
+    private final String login;
+    private final String password;
 
-    public Request(String requestId, CommandType commandType, CommandArgument commandArgument) {
+    public Request(String requestId, CommandType commandType, CommandArgument commandArgument, String login, String password) {
         this.commandType = commandType;
         this.commandArgument = commandArgument;
         this.requestId = requestId;
+        this.login = login;
+        this.password = password;
     }
 
     public String getRequestId() {
         return requestId;
     }
+
+    public String getLogin() {return login;}
+
+    public String getPassword() {return password;}
 
     public CommandType getCommandType() {
         return commandType;
